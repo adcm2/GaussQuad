4325aac47f39649df76a468c8fc1f66ba3756143
David <da380@cam.ac.uk>
2023-07-13 14:45:37 +0100

    restructuring

--- GaussQuad.h
+++
@@ -1,8 +0,0 @@
-#ifndef GAUSS_QUAD_MODULE_H
-#define GAUSS_QUAD_MODULE_H
-
-// Header files to be included to use the GaussQuad library.
-#include "src/OrthogonalPolynomial.h"
-#include "src/Quadrature.h"
-
-#endif  
