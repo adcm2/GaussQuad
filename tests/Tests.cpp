f7e6cf397611ae5c8b42df7bad6d2152ae902f42
David <da380@cam.ac.uk>
2023-09-19 14:20:00 +0100

    tests updated

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -1,102 +1,137 @@
 #include <gtest/gtest.h>
 
 #include <GaussQuad/All>
+#include <Interp/Polynomial>
 #include <complex>
 #include <concepts>
 #include <limits>
-
-// set some data.
-constexpr int n = 10;
+#include <random>
 
 template <std::floating_point Float>
-constexpr Float exact = static_cast<Float>(2.0) / static_cast<Float>(3.0);
+constexpr auto eps = 2000 * std::numeric_limits<Float>::epsilon();
 
-template <std::floating_point Float>
-constexpr auto eps = 100 * std::numeric_limits<Float>::epsilon();
-
-template <std::floating_point Float>
-auto RealFunction = [](Float x) -> Float { return x * x; };
-
-template <std::floating_point Float>
-auto ComplexFunction = [](Float x) -> std::complex<Float> {
-  return std::complex<Float>{x * x, x * x};
-};
-
-template <std::floating_point Float>
+template <std::floating_point Float, bool Complex = false>
 int TestGauss(int n) {
-  using Complex = std::complex<Float>;
   auto q = GaussQuad::GaussLegendreQuadrature1D<Float>(n);
-  Float error1 = std::abs(q.Integrate(RealFunction<Float>) - exact<Float>);
-  Float error2 = std::abs(q.Integrate(ComplexFunction<Float>) -
-                          Complex{exact<Float>, exact<Float>});
-
-  return (error1 < eps<Float> and error2 < eps<Float>) ? 0 : 1;
+  int m = 2 * n - 1;
+  if constexpr (Complex) {
+    auto p = Interp::Polynomial1D<std::complex<Float>>::Random(m);
+    Float error = std::abs(q.Integrate(p) - p.Integrate(-1, 1));
+    return (error < eps<Float>) ? 0 : 1;
+  } else {
+    auto p = Interp::Polynomial1D<Float>::Random(m);
+    Float error = std::abs(q.Integrate(p) - p.Integrate(-1, 1));
+    return (error < eps<Float>) ? 0 : 1;
+  }
 }
 
-template <std::floating_point Float>
+template <std::floating_point Float, bool Complex = false>
 int TestRadau(int n) {
-  using Complex = std::complex<Float>;
   auto q = GaussQuad::GaussRadauLegendreQuadrature1D<Float>(n);
-  Float error1 = std::abs(q.Integrate(RealFunction<Float>) - exact<Float>);
-  Float error2 = std::abs(q.Integrate(ComplexFunction<Float>) -
-                          Complex{exact<Float>, exact<Float>});
-
-  return (error1 < eps<Float> and error2 < eps<Float>) ? 0 : 1;
+  int m = 2 * n - 3;
+  if constexpr (Complex) {
+    auto p = Interp::Polynomial1D<std::complex<Float>>::Random(m);
+    Float error = std::abs(q.Integrate(p) - p.Integrate(-1, 1));
+    return (error < eps<Float>) ? 0 : 1;
+  } else {
+    auto p = Interp::Polynomial1D<Float>::Random(m);
+    Float error = std::abs(q.Integrate(p) - p.Integrate(-1, 1));
+    return (error < eps<Float>) ? 0 : 1;
+  }
 }
 
-template <std::floating_point Float>
+template <std::floating_point Float, bool Complex = false>
 int TestLobatto(int n) {
-  using Complex = std::complex<Float>;
   auto q = GaussQuad::GaussLobattoLegendreQuadrature1D<Float>(n);
-  Float error1 = std::abs(q.Integrate(RealFunction<Float>) - exact<Float>);
-  Float error2 = std::abs(q.Integrate(ComplexFunction<Float>) -
-                          Complex{exact<Float>, exact<Float>});
+  int m = 2 * n - 3;
+  if constexpr (Complex) {
+    auto p = Interp::Polynomial1D<std::complex<Float>>::Random(m);
+    Float error = std::abs(q.Integrate(p) - p.Integrate(-1, 1));
+    return (error < eps<Float>) ? 0 : 1;
+  } else {
+    auto p = Interp::Polynomial1D<Float>::Random(m);
+    Float error = std::abs(q.Integrate(p) - p.Integrate(-1, 1));
+    return (error < eps<Float>) ? 0 : 1;
+  }
+}
 
-  return (error1 < eps<Float> and error2 < eps<Float>) ? 0 : 1;
+int RandomDegree() {
+  std::random_device rd{};
+  std::mt19937_64 gen{rd()};
+  std::uniform_int_distribution d{5, 200};
+  return d(gen);
 }
 
 // Set the tests.
-TEST(Gauss, Float) {
-  int i = TestGauss<float>(n);
-  EXPECT_EQ(i, 0);
-}
 
-TEST(Gauss, Double) {
+TEST(Gauss, RealDouble) {
+  int n = RandomDegree();
   int i = TestGauss<double>(n);
   EXPECT_EQ(i, 0);
 }
 
-TEST(Gauss, LongDouble) {
+TEST(Gauss, ComplexDouble) {
+  int n = RandomDegree();
+  int i = TestGauss<double, true>(n);
+  EXPECT_EQ(i, 0);
+}
+
+TEST(Gauss, RealLongDouble) {
+  int n = RandomDegree();
   int i = TestGauss<long double>(n);
   EXPECT_EQ(i, 0);
 }
 
-TEST(Radau, Float) {
-  int i = TestRadau<float>(n);
+TEST(Gauss, ComplexLongDouble) {
+  int n = RandomDegree();
+  int i = TestGauss<long double, true>(n);
   EXPECT_EQ(i, 0);
 }
 
-TEST(Radau, Double) {
+TEST(Radau, RealDouble) {
+  int n = RandomDegree();
   int i = TestRadau<double>(n);
   EXPECT_EQ(i, 0);
 }
 
-TEST(Radau, LongDouble) {
+TEST(Radau, ComplexDouble) {
+  int n = RandomDegree();
+  int i = TestRadau<double, true>(n);
+  EXPECT_EQ(i, 0);
+}
+
+TEST(Radau, RealLongDouble) {
+  int n = RandomDegree();
   int i = TestRadau<long double>(n);
   EXPECT_EQ(i, 0);
 }
 
-TEST(Lobatto, Float) {
-  int i = TestLobatto<float>(n);
+TEST(Radau, ComplexLongDouble) {
+  int n = RandomDegree();
+  int i = TestRadau<long double, true>(n);
   EXPECT_EQ(i, 0);
 }
 
-TEST(Lobatto, Double) {
+TEST(Lobatto, RealDouble) {
+  int n = RandomDegree();
   int i = TestLobatto<double>(n);
   EXPECT_EQ(i, 0);
 }
 
-TEST(Lobatto, LongDouble) {
+TEST(Lobatto, ComplexDouble) {
+  int n = RandomDegree();
+  int i = TestLobatto<double, true>(n);
+  EXPECT_EQ(i, 0);
+}
+
+TEST(Lobatto, RealLongDouble) {
+  int n = RandomDegree();
   int i = TestLobatto<long double>(n);
+  EXPECT_EQ(i, 0);
+}
+
+TEST(Lobatto, ComplexLongDouble) {
+  int n = RandomDegree();
+  int i = TestLobatto<long double, true>(n);
   EXPECT_EQ(i, 0);
 }
